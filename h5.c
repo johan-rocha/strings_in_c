b5f4c6d812a4ba8f52580e9bfbd6535602edccde
Johan Rocha <johanmarcos50@gmail.com>
2023-01-05 21:54:27 -0300

    simplificado

--- h5.c
+++ h5.c
@@ -1,103 +1,73 @@
 #include <stdio.h>
 
-/* Passos:
-    1 - conta quantos caracteres tem em cada palavra
-    2 - acha posições do final de cada palavra contida no texto
-    3 - a partir da palavra, desloca todos os caracteres em (qteCaracterPalavraAInserir - qteCaracterPalavraContido)
-    4 - insere a palavra a partir da posição do caractere que havia no texto
-*/
-
-int countCaracter(char str[41]); //podera ser usada caso a funcao countCaracter nao funcione
-int positionWord(char frase[], char p1[], int cur_position); //acha a palavra e dá a posição dela com base na posicao especificada da string
-void replaceWord(char frase[], char p1[], char p2[]); //substitui a palavra
+/*-----------------------FUNCTIONS PROTOTYPES----------------------------*/
+int countCaracter(char str[41]);
+void replaceWord(char phrase[], char text_word[], char to_replace_word[]);
+/*-----------------------------------------------------------------------*/
 
 int main()
 {
-    char frase[1001], p1[41], p2[41]; //p1 - palavra contida ; p2 - palavra a substituir
-
-    scanf("%[^\n]s", frase);
-    getchar();
-    scanf("%[^\n]s", p1);
-    getchar();
-    scanf("%[^\n]s", p2);
-    getchar();
-    replaceWord(frase, p1, p2);
-    printf("%s\n", frase);
-
-    return 0;
+    char phrase[1001], text_word[41], to_replace_word[41];
+    scanf(" %[^\n]s", phrase);
+    scanf(" %[^\n]s", text_word);
+    scanf(" %[^\n]s", to_replace_word);
+    replaceWord(phrase, text_word, to_replace_word);
+    printf("%s\n", phrase);
 }
 
-void replaceWord(char frase[], char p1[], char p2[])
+int countCaracter(char str[41])
 {
-    int sizep1, sizep2, next_word_end_position, deslocamento;
-    sizep1 = countCaracter(p1);
-    sizep2 = countCaracter(p2);
-    next_word_end_position = positionWord(frase, p1, 0); //posicao do caractere apos a palavra
-
-    while(next_word_end_position != -1)
-    {
-        if(sizep1 < sizep2)
-        {
-            for(int i=countCaracter(frase); i >= next_word_end_position; i--) //desloca conjunto de palavras incluindo o caractere \0
-            { 
-                frase[i+(sizep2 - sizep1)] = frase[i];
-                frase[i] = ' ';
-            }
-            deslocamento = (sizep2 - sizep1);
-        }
-        else if(sizep1 > sizep2)
-        {
-            for(int i=(next_word_end_position - (sizep1 - sizep2)); i <= countCaracter(frase) - (sizep1 - sizep2); i++)
-            {
-                frase[i] = frase[i + (sizep1 - sizep2)];
-            }
-            deslocamento = (sizep2 - sizep1);
-        }
-        else
-            deslocamento = 0;
-
-        int count = (sizep2-1);
-        for(int i=((next_word_end_position+deslocamento)-1);i > ((next_word_end_position+deslocamento)-1) - sizep2; i--) //substitui com ele deslocado
-        {
-            frase[i] = p2[count];
-            count --;
-        }
-        next_word_end_position = positionWord(frase, p1, next_word_end_position+deslocamento);
-    }
-}
-
-int countCaracter(char str[41]) // editar depois
-{
-    int tam = 0;
-    for(tam; str[tam] != '\0'; tam++)
-    {
-        if(str[tam] == '\n')
-        {
-            str[tam] = '\0';
-            tam = 0;
-        }
-    }
+    int tam;
+    for(tam = 0; str[tam] != '\0'; tam++);
     return tam;
 }
 
-int positionWord(char frase[], char p1[], int cur_position) //cur position e a partir de onde sera analisado
+void replaceWord(char phrase[], char text_word[], char to_replace_word[])
 {
+    int size_text_word = countCaracter(text_word);
+    int size_to_replace_word = countCaracter(to_replace_word);
+    int size_phrase = countCaracter(phrase);
+    int desloc = size_to_replace_word - size_text_word;
+    
     int count = 0;
-
-    for(int i=cur_position; i < countCaracter(frase); i++)
+    for(int i=0; phrase[i] != '\0'; i++)
     {
-        if(frase[i] == p1[count])
+        if (phrase[i] == text_word[count])
         {
             count++;
-            if(count == countCaracter(p1))
+            if(count == size_text_word)
             {
-                return (i+1);
+                //faz o deslocamento. Em cada caso há um sentido de varredura do vetor
+                if(size_text_word < size_to_replace_word)//nesse caso, tem que aumentar o tamanho de espacos
+                {
+                    //o tamanho da string(size_phrase) coincide com a posicao de phrase ate o \0
+                    for(int j=size_phrase; j >= (i+1); j--) //i+1 é a posicao do caractere após a palavra
+                    {
+                        phrase[j+desloc] = phrase[j];
+                        phrase[j] = ' ';
+                    }
+                }
+                else if(size_text_word > size_to_replace_word)//nesse caso, tem que diminuir o tamanho de espacos
+                {
+                    for(int j=(i+1); j <= size_phrase; j++) //i+1 é a posicao do caractere após a palavra
+                    {
+                        phrase[j+desloc] = phrase[j];
+                    }
+                }
+                
+                //faz a substituicao do final do deslocamento ate o inicio da palavra
+                int count2 = (size_to_replace_word-1);
+                for(int j=i+desloc; j >=(i+desloc)-(size_to_replace_word-1); j--)
+                {
+                    phrase[j] = to_replace_word[count2];
+                    count2--;
+                }
+                size_phrase = countCaracter(phrase); //o tamanho da frase vai alterando
             }
         }
         else
+        {
             count = 0;
+        }
     }
-
-    return -1; //quando nao for mais possivel entrar no laco for, a funcao retorna -1 para indicar o fim da string
 }
-
